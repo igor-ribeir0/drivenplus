b2103dab128ce64a99fbe0a67260382b9ce37d8f
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2022-12-23 17:48:58 -0300

    created: layout confirm signature

--- src/components/pages/StatusPlanPage.js
+++ src/components/pages/StatusPlanPage.js
@@ -1,6 +1,5 @@
 import { Data, Input, Button } from '../../styles/constants/styledComponents';
 import styled from 'styled-components';
-import logoPlus from '../../assets/imgs/logoPlus.svg';
 import clipBoard from '../../assets/imgs/clipboard.svg';
 import money from '../../assets/imgs/money.svg';
 import { useParams } from 'react-router-dom';
@@ -17,6 +16,10 @@ export default function StatusPlanPage(){
     const [imageName, setImageName] = useState('');
     const [price, setPrice] = useState('');
     const [benefitsList, setBenefitsList] = useState([]);
+    const [nameCardOwner, setNameCardOwner] = useState('');
+    const [cardNumber, setCardNumber] = useState('');
+    const [cardSafetyPassword, setCardSafetyPassword] = useState();
+    const [shelfLife, setShelfLife] = useState('');
 
     const config = {
         headers: {
@@ -35,6 +38,10 @@ export default function StatusPlanPage(){
         setImageName(answer.image);
         setPrice(answer.price);
         setBenefitsList(answer.perks);
+    };
+
+    function signature(event){
+        event.preventDefault();
     };
 
     return(
@@ -69,34 +76,56 @@ export default function StatusPlanPage(){
             </Benefit>
 
             <CardData>
-                <form>
+                <form onSubmit={signature}>
                     <Input 
                         type='text'
+                        value={nameCardOwner}
+                        onChange={e => setNameCardOwner(e.target.value)}
                         placeholder='Nome impresso no cartão'
                         required
                     />
                     <Input 
-                        type='number'
+                        type='text'
+                        value={cardNumber}
+                        onChange={e => setCardNumber(e.target.value)}
                         placeholder='Digitos do cartão'
                         required
                     /> 
                     <div>
                         <CardKey 
                             type='number'
+                            value={cardSafetyPassword}
+                            onChange={e => setCardSafetyPassword(e.target.value)}
                             placeholder='Código de Segurança'
                             required
                         />
                         <CardKey 
-                            type='number'
+                            type='text'
+                            value={shelfLife}
+                            onChange={e => setShelfLife(e.target.value)}
                             placeholder='Validade'
                             required
                         />
                     </div>
-                    <Button>
+                    <Button type='submit'>
                         ASSINAR
                     </Button>
                 </form>
             </CardData>
+
+            <ConfirmSignature>
+                <div>
+                    <ContentConfirm>
+                        <h2>Tem certeza que deseja</h2>
+                        <h3>assinar o plano</h3>
+                        <h2>{planTitle} (R$ {price})?</h2>
+
+                        <NoButton>Não</NoButton>
+                        <YesButton>SIM</YesButton>
+                    </ContentConfirm>
+                </div>
+            </ConfirmSignature>
+
         </Data>
     );
 };
@@ -202,4 +231,85 @@ appearance: textfield;
 &::-webkit-inner-spin-button { 
     -webkit-appearance: none;
 }
+`;
+
+const ConfirmSignature = styled.div`
+width: 100%;
+display: flex;
+justify-content: center;
+align-items: center;
+    div{
+        width: 250px;
+        height: 230px;
+        background-color: #FFFFFF;
+        border-radius: 10px;
+        position: absolute;
+        bottom: 0;
+        left: auto;
+        display: flex;
+        flex-direction: column;
+    }
+`;
+
+const ContentConfirm = styled.header`
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+    h2{
+        width: 60px;
+        height: 30px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+        margin-top: 30px;
+        margin-left: -160px;
+        white-space: nowrap;
+    }
+    h3{
+        width: 60px;
+        height: 30px;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 700;
+        font-size: 20px;
+        line-height: 20px;
+        color: #000000;
+        margin-top: -5px;
+        margin-bottom: -33px;
+        margin-left: -90px;
+        white-space: nowrap;
+    }
+`;
+
+const NoButton = styled.button`
+width: 100px;
+height: 60px;
+font-family: 'Roboto', sans-serif;
+font-weight: 700;
+font-size: 12px;
+line-height: 10px;
+color: #FFFFFF;
+background-color: #7E7E7E;
+border-radius: 6px;
+border: none;
+margin-top: 50px;
+margin-left: -110px;
+`;
+
+const YesButton = styled.button`
+width: 100px;
+height: 60px;
+font-family: 'Roboto', sans-serif;
+font-weight: 700;
+font-size: 12px;
+line-height: 10px;
+color: #FFFFFF;
+background-color: #FF4791;
+border-radius: 6px;
+border: none;
+margin-top: -59px;
+margin-left: 120px;
 `;
